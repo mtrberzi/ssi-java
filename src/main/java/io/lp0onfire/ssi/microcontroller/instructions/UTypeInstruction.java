725d799dd84d7472e5ea21280aa5271bd7ccfb7a
Murphy Berzish <murphy.berzish@gmail.com>
2015-06-08 17:56:36 -0400

    decode U-type instructions

---
+++ src/main/java/io/lp0onfire/ssi/microcontroller/instructions/UTypeInstruction.java
@@ -0,0 +1,26 @@
+package io.lp0onfire.ssi.microcontroller.instructions;
+
+import io.lp0onfire.ssi.microcontroller.RV32Instruction;
+
+public abstract class UTypeInstruction extends RV32Instruction {
+
+  protected final int imm;
+  public int getImm() {
+    return this.imm;
+  }
+  protected final int rd;
+  public int getRd() {
+    return this.rd;
+  }
+  protected final int opcode;
+  public int getOpcode() {
+    return this.opcode;
+  }
+  
+  public UTypeInstruction(int instruction) {
+    imm = (instruction & 0b11111111111111111111000000000000);
+    rd = (instruction & 0b00000000000000000000111110000000) >>> 7;
+    opcode = (instruction & 0b00000000000000000000000001111111);
+  }
+  
+}
