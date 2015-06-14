49c7fad24d93fd65f2b5471f78c69f1196e8d1ea
Murphy Berzish <murphy.berzish@gmail.com>
2015-06-14 17:19:23 -0400

    test ROM reads

--- src/main/java/io/lp0onfire/ssi/microcontroller/peripherals/ROM.java
+++ src/main/java/io/lp0onfire/ssi/microcontroller/peripherals/ROM.java
@@ -18,44 +18,62 @@ public class ROM implements SystemBusPeripheral {
     return this.numberOfPages;
   }
 
+  public void setContents(byte[] contents) {
+    if (contents.length != memory.length) {
+      throw new IllegalArgumentException();
+    }
+    System.arraycopy(contents, 0, memory, 0, memory.length);
+  }
+  
   @Override
   public int readByte(int address) throws AddressTrapException {
     int localAddr = translateAddress(address);
     if (localAddr >= 0 && localAddr < memory.length) {
-      return memory[localAddr];
+      return ((int)memory[localAddr]) & 0x0FF;
+    } else {
+      return 0;
+    }
+  }
+
+  // precondition: address is halfword aligned
+  @Override
+  public int readHalfword(int address) throws AddressTrapException {
+    int localAddr = translateAddress(address);
+    if (localAddr >= 0 && localAddr+1 < memory.length) {
+      return
+          (memory[localAddr+0] & 0x000000FF) |
+          (memory[localAddr+1] & 0x000000FF) << 8
+          ;
     } else {
       return 0;
     }
   }
 
   @Override
-  public int readHalfword(int address) throws AddressTrapException {
-    // TODO Auto-generated method stub
-    return 0;
-  }
-
-  @Override
   public int readWord(int address) throws AddressTrapException {
-    // TODO Auto-generated method stub
-    return 0;
+    int localAddr = translateAddress(address);
+    if (localAddr >= 0 && localAddr+3 < memory.length) {
+      return
+          (memory[localAddr+0] & 0x000000FF) |
+          (memory[localAddr+1] & 0x000000FF) <<  8 |
+          (memory[localAddr+2] & 0x000000FF) << 16 |
+          (memory[localAddr+3] & 0x000000FF) << 24
+          ;
+    } else {
+      return 0;
+    }
   }
 
   @Override
   public void writeByte(int address, int value) throws AddressTrapException {
-    // TODO Auto-generated method stub
-    
   }
 
   @Override
   public void writeHalfword(int address, int value) throws AddressTrapException {
-    // TODO Auto-generated method stub
-    
   }
 
   @Override
   public void writeWord(int address, int value) throws AddressTrapException {
-    // TODO Auto-generated method stub
-    
   }
 
 }
