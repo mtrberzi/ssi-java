7bfa1f8aa831a8f663bb841c9c61dc7d5acfc3e5
Murphy Berzish <murphy.berzish@gmail.com>
2015-07-20 19:12:27 -0400

    don't clear registers in startup code; functions to disable/enable interrupts

--- runtime/src/startup.s
+++ runtime/src/startup.s
@@ -10,43 +10,12 @@ machine_trap_entry:	j trap_entry
 	.align 6
 	.globl _start
 _start:
-	## initialize all registers to 0
-	## except the stack pointer
-	li x1, 0
+	## initalize stack pointer
 	la sp, stack_top
-	li x3, 0
-	li x4, 0
-	li x5, 0
-	li x6, 0
-	li x7, 0
-	li x8, 0
-	li x9, 0
-	li x10, 0
-	li x11, 0
-	li x12, 0
-	li x13, 0
-	li x14, 0
-	li x15, 0
-	li x16, 0
-	li x17, 0
-	li x18, 0
-	li x19, 0
-	li x20, 0
-	li x21, 0
-	li x22, 0
-	li x23, 0
-	li x24, 0
-	li x25, 0
-	li x26, 0
-	li x27, 0
-	li x28, 0
-	li x29, 0
-	li x30, 0
-	li x31, 0
-	## call main
 	la t0, main
-	jr t0
-
+	jalr t0
+loop:	j loop
+	
 trap_entry:
 	addi sp, sp, -128
 	sw x1, 1*4(sp)
@@ -121,3 +90,14 @@ trap_entry:
 	
 	addi sp, sp, 128
 	sret
+
+	.globl enable_interrupts
+enable_interrupts:
+	csrrsi x0, 0x300, 1	# set bit 1 of mstatus
+	ret
+
+	.globl disable_interrupts
+disable_interrupts:
+	csrrci x0, 0x300, 1	# clear bit 1 of mstatus
+	ret
+	
