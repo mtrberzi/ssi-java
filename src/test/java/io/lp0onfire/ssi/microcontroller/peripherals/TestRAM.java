44a0fbe208cdf05de18277d426ae2c7c86fd1a5f
Murphy Berzish <murphy.berzish@gmail.com>
2015-08-02 14:14:10 -0400

    ROM and RAM tests no longer write to static field testContents

--- src/test/java/io/lp0onfire/ssi/microcontroller/peripherals/TestRAM.java
+++ src/test/java/io/lp0onfire/ssi/microcontroller/peripherals/TestRAM.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 
 public class TestRAM {
 
-  private static byte[] testContents;
+  private byte[] testContents;
   private RAM ram;
   
   @Before
--- src/test/java/io/lp0onfire/ssi/microcontroller/peripherals/TestROM.java
+++ src/test/java/io/lp0onfire/ssi/microcontroller/peripherals/TestROM.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 
 public class TestROM {
 
-  private static byte[] testContents;
+  private byte[] testContents;
   private ROM rom;
   
   @Before
