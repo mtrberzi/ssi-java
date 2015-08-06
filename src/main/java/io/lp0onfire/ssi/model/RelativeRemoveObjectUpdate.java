f354c86efe8120c4c7b702be5290a55919e2f5a3
Murphy Berzish <murphy.berzish@gmail.com>
2015-08-06 18:53:48 -0400

    relative remove object world update

---
+++ src/main/java/io/lp0onfire/ssi/model/RelativeRemoveObjectUpdate.java
@@ -0,0 +1,25 @@
+package io.lp0onfire.ssi.model;
+
+public class RelativeRemoveObjectUpdate extends WorldUpdate {
+  
+  private final VoxelOccupant baseObject;
+  private final Vector displacement;
+  private final VoxelOccupant removedObject;
+  
+  public RelativeRemoveObjectUpdate(VoxelOccupant baseObject, Vector displacement, VoxelOccupant removedObject) {
+    this.baseObject = baseObject;
+    this.displacement = displacement;
+    this.removedObject = removedObject;
+  }
+  
+  @Override
+  public WorldUpdateResult apply(World w) {
+    Vector position = baseObject.getPosition().add(displacement);
+    if (!w.getOccupants(position).contains(removedObject)) {
+      // failure: removed object isn't there
+      return new WorldUpdateResult(false);
+    }
+    w.removeOccupant(removedObject);
+    return new WorldUpdateResult(true);
+  }
+}
