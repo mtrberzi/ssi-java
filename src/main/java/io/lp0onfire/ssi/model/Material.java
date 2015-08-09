5e7f323a94485106eb41af30c416f99672c1dd12
Murphy Berzish <murphy.berzish@gmail.com>
2015-08-09 17:32:41 -0400

    material attributes for smelting

--- src/main/java/io/lp0onfire/ssi/model/Material.java
+++ src/main/java/io/lp0onfire/ssi/model/Material.java
@@ -1,5 +1,25 @@
 package io.lp0onfire.ssi.model;
 
 public class Material {
-
+  private final boolean canBeSmelted;
+  public boolean getCanBeSmelted() {
+    return this.canBeSmelted;
+  }
+  
+  private final int smeltingTimesteps;
+  public int getSmeltingTimesteps() {
+    return this.smeltingTimesteps;
+  }
+  
+  private final int numberOfSmeltedBars;
+  public int getNumberOfSmeltedBars() {
+    return this.numberOfSmeltedBars;
+  }
+  
+  public Material(boolean canBeSmelted, int smeltingTimesteps, int numberOfSmeltedBars) {
+    this.canBeSmelted = canBeSmelted;
+    this.smeltingTimesteps = smeltingTimesteps;
+    this.numberOfSmeltedBars = numberOfSmeltedBars;
+  }
+  
 }
--- src/main/java/io/lp0onfire/ssi/model/MaterialBuilder.java
+++ src/main/java/io/lp0onfire/ssi/model/MaterialBuilder.java
@@ -2,8 +2,31 @@ package io.lp0onfire.ssi.model;
 
 public class MaterialBuilder {
 
+  private boolean canBeSmelted = false;
+  public void setCanBeSmelted(boolean b) {
+    this.canBeSmelted = b;
+  }
+  
+  private int smeltingTimesteps = -1;
+  public void setSmeltingTimesteps(int i) {
+    this.smeltingTimesteps = i;
+  }
+  
+  private int numberOfSmeltedBars = -1;
+  public void setNumberOfSmeltedBars(int i) {
+    this.numberOfSmeltedBars = i;
+  }
+  
   public Material build() {
-    return new Material();
+    if (canBeSmelted) {
+      if (smeltingTimesteps < 0) {
+        throw new IllegalArgumentException("must specify number of smelting timesteps");
+      }
+      if (numberOfSmeltedBars < 0) {
+        throw new IllegalArgumentException("must specify number of smelted bars");
+      }
+    }
+    return new Material(canBeSmelted, smeltingTimesteps, numberOfSmeltedBars);
   }
   
 }
